f099b7c9456abea3bf4da96efde09812357bfa96
SirSantana <salazarmiguelito23@gmail.com>
2025-04-20 23:29:49 -0500

    MejoraUIDashboard3

--- app/carro/page.tsx
+++
@@ -1,9 +0,0 @@
-import EngineQuiz from "../components/CarroComponent";
-
-export default function Home() {
-  return (
-<main className="bg-black min-h-screen">
-      <EngineQuiz />
-    </main>
-  );
-}
